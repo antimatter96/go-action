36a7e0c2bc68c89ff6beb204c994d790b9dbc463
Arpit Jain <me.arpitjain09@gmail.com>
2021-12-13 12:08:54 +0530

    rocket

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/pkg/errors"
 	flag "github.com/spf13/pflag"
 
-	"github.com/the-gophers/go-action/pkg/tweeter"
+	"github.com/antimatter96/go-action/pkg/tweeter"
 )
 
 var (
--- pkg/tweeter/tweeter_test.go
+++ pkg/tweeter/tweeter_test.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/the-gophers/go-action/pkg/tweeter"
+	"github.com/antimatter96/go-action/pkg/tweeter"
 )
 
 func TestNew(t *testing.T) {
